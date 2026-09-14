b7c20f363e28d9a62c00ef221915e77f449b119f
Vinogikan Manimaran <it25103946@my.sliit.lk>
2026-09-14 11:50:48 +0530

    medicine class

--- src/main/java/com/medilink/entity/Batch.java
+++ src/main/java/com/medilink/entity/Batch.java
@@ -1,4 +1,65 @@
 package com.medilink.entity;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Batch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "medicine_id")
+    private Medicine medicine;
+
+    private String batchNumber;
+    private int quantity;
+    private LocalDate expiryDate;
+    private boolean locked;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Medicine getMedicine() {
+        return medicine;
+    }
+
+    public void setMedicine(Medicine medicine) {
+        this.medicine = medicine;
+    }
+
+    public String getBatchNumber() {
+        return batchNumber;
+    }
+
+    public void setBatchNumber(String batchNumber) {
+        this.batchNumber = batchNumber;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(LocalDate expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
 }
